a80daef8c0b51444eaca9fe97bf0dac3d46f5565
bradl <bradleyevans.x@gmail.com>
2023-11-17 12:47:48 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <Button>Test</Button>
+    </>
+  );
+}
+
+export default App;
